421a7729406526dcd8325113909e29dd9f789036
Trim21 <i@trim21.me>
2020-04-25 06:31:23 +0800

    chore: move dist file to gh-pages

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,6 @@
 function request (url) {
   return new Promise((resolve, reject) => {
+    console.log('rew')
     window.GM_xmlhttpRequest({
       url,
       onload: function ({ response, responseText, }) {
