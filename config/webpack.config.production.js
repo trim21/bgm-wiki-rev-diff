8e154ce94221283719db7ba26eff30b3eb2d267f
Trim21 <i@trim21.me>
2020-04-26 05:11:57 +0800

    build: minify final script

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -1,12 +1,23 @@
 const merge = require('webpack-merge')
-
+const TerserPlugin = require('terser-webpack-plugin')
+const UserScriptMetaDataPlugin = require('userscript-metadata-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const UserScriptMetaDataPlugin = require('userscript-metadata-webpack-plugin')
 const metadata = require('./metadata')
-
 const webpackConfig = require('./webpack.config.base')
+
 const cfg = merge({}, webpackConfig, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false, // Must be set to true if using source-maps in production
+      }),
+    ],
+  },
   output: {
     filename: metadata.name + '.prod.user.js'
   },
