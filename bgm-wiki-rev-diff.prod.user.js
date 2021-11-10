729b5b750019cfc8bb5fa4cc0864dace706d3ea3
Trim21 <Trim21@users.noreply.github.com>
2021-11-10 06:09:35 +0000

    deploy refs/tags/v0.1.8 458f0102e96933ba41bbd6f101a8d2d2412d85ec

--- bgm-wiki-rev-diff.prod.user.js
+++ bgm-wiki-rev-diff.prod.user.js
@@ -2,7 +2,7 @@
 // @name         bgm-wiki-rev-diff
 // @name:zh      显示条目信息版本差异
 // @namespace    https://trim21.me/
-// @version      0.1.7
+// @version      0.1.8
 // @author       Trim21 <i@trim21.me>
 // @source       https://github.com/Trim21/bgm-wiki-rev-diff
 // @supportURL   https://github.com/Trim21/bgm-wiki-rev-diff/issues
@@ -13,190 +13,205 @@
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/diff2html@3.4.12/bundles/js/diff2html.min.js
 // @require      https://cdn.jsdelivr.net/npm/diff@5.0.0/dist/diff.min.js
-// @require      https://cdn.jsdelivr.net/npm/lodash@^4.17.21/lodash.min.js
+// @require      https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
 // @resource     diff2html https://cdn.jsdelivr.net/npm/diff2html@3.4.12/bundles/css/diff2html.min.css
 // @grant        GM.getResourceUrl
 // @run-at       document-end
 // ==/UserScript==
 
 
-/******/ (function() { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
 ;// CONCATENATED MODULE: external "$"
-var external_$_namespaceObject = $;
+const external_$_namespaceObject = $;
 ;// CONCATENATED MODULE: external "_"
-var external_namespaceObject = _;
+const external_namespaceObject = _;
 ;// CONCATENATED MODULE: ./src/parser.ts
 
 function parseRevDetails(html) {
-    const jq = external_$_namespaceObject(html);
-    const rawInfo = jq.find('#subject_infobox').val()?.toString() ?? '';
-    const title = jq.find('input[name="subject_title"]').val()?.toString() ?? '';
-    const description = jq.find('textarea#subject_summary').val()?.toString() ?? '';
-    return {
-        title,
-        rawInfo,
-        description,
-    };
+  var _jq$find$val, _jq$find$val2, _jq$find$val3;
+
+  const jq = external_$_namespaceObject(html);
+  const rawInfo = ((_jq$find$val = jq.find('#subject_infobox').val()) === null || _jq$find$val === void 0 ? void 0 : _jq$find$val.toString()) ?? '';
+  const title = ((_jq$find$val2 = jq.find('input[name="subject_title"]').val()) === null || _jq$find$val2 === void 0 ? void 0 : _jq$find$val2.toString()) ?? '';
+  const description = ((_jq$find$val3 = jq.find('textarea#subject_summary').val()) === null || _jq$find$val3 === void 0 ? void 0 : _jq$find$val3.toString()) ?? '';
+  return {
+    title,
+    rawInfo,
+    description
+  };
 }
 function parseRevEl(el) {
-    const date = el.find('a:not(.compare-previous-trim21-cn)').first().html();
-    const revEL = el.find('a.l:contains("恢复")');
-    const revCommentEl = el.find('span.comment');
-    let comment = '';
-    if (revCommentEl.length > 0) {
-        comment = revCommentEl.html();
-        comment = comment.substring(1, comment.length - 1);
-    }
-    const revHref = revEL.attr('href');
-    if (!revHref) {
-        // this is a merge commit, can't know what's really info
-        return undefined;
-    }
-    const revID = revHref.split('/').pop();
-    if (!revID) {
-        throw new Error(`can't parse rev id from ${revHref}`);
-    }
-    return {
-        id: revID,
-        comment,
-        date,
-        url: revHref,
-    };
-}
-function getRevs() {
-    const revs = [];
-    external_$_namespaceObject('#pagehistory li').each(function () {
-        const rev = parseRevEl(external_$_namespaceObject(this));
-        if (rev) {
-            revs.push(rev);
-        }
-    });
-    return revs;
-}
-function getRevInfo(revID) {
-    for (const rev of getRevs()) {
-        if (rev.id === revID) {
-            return rev;
-        }
-    }
+  const date = el.find('a:not(.compare-previous-trim21-cn)').first().html();
+  const revEL = el.find('a.l:contains("恢复")');
+  const revCommentEl = el.find('span.comment');
+  let comment = '';
+
+  if (revCommentEl.length > 0) {
+    comment = revCommentEl.html();
+    comment = comment.substring(1, comment.length - 1);
+  }
+
+  const revHref = revEL.attr('href');
+
+  if (!revHref) {
+    // this is a merge commit, can't know what's really info
+    return undefined;
+  }
+
+  const revID = revHref.split('/').pop();
+
+  if (!revID) {
+    throw new Error(`can't parse rev id from ${revHref}`);
+  }
+
+  return {
+    id: revID,
+    comment,
+    date,
+    url: revHref
+  };
 }
 
+function getRevs() {
+  const revs = [];
+  external_$_namespaceObject('#pagehistory li').each(function () {
+    const rev = parseRevEl(external_$_namespaceObject(this));
+
+    if (rev) {
+      revs.push(rev);
+    }
+  });
+  return revs;
+}
+
+function getRevInfo(revID) {
+  for (const rev of getRevs()) {
+    if (rev.id === revID) {
+      return rev;
+    }
+  }
+}
 ;// CONCATENATED MODULE: external "Diff"
-var external_Diff_namespaceObject = Diff;
+const external_Diff_namespaceObject = Diff;
 ;// CONCATENATED MODULE: ./src/differ.ts
 
 function diff(revOld, revNew) {
-    return [
-        titleDiff(revOld, revNew),
-        infoDiff(revOld, revNew),
-        descriptionDiff(revOld, revNew),
-    ].join('\n');
-}
-function titleDiff(rev1, rev2) {
-    if (rev1.details.title === rev2.details.title) {
-        return '';
-    }
-    return external_Diff_namespaceObject.createPatch('条目名', rev1.details.title, rev2.details.title, rev1.rev.date, rev2.rev.date);
-}
-function infoDiff(rev1, rev2) {
-    if (rev1.details.rawInfo === rev2.details.rawInfo) {
-        return '';
-    }
-    return external_Diff_namespaceObject.createPatch('相关信息', rev1.details.rawInfo, rev2.details.rawInfo, rev1.rev.date, rev2.rev.date);
-}
-function descriptionDiff(rev1, rev2) {
-    if (rev1.details.description === rev2.details.description) {
-        return '';
-    }
-    return external_Diff_namespaceObject.createPatch('简介', rev1.details.description, rev2.details.description, rev1.rev.date, rev2.rev.date);
+  return [titleDiff(revOld, revNew), infoDiff(revOld, revNew), descriptionDiff(revOld, revNew)].join('\n');
 }
 
+function titleDiff(rev1, rev2) {
+  if (rev1.details.title === rev2.details.title) {
+    return '';
+  }
+
+  return external_Diff_namespaceObject.createPatch('条目名', rev1.details.title, rev2.details.title, rev1.rev.date, rev2.rev.date);
+}
+
+function infoDiff(rev1, rev2) {
+  if (rev1.details.rawInfo === rev2.details.rawInfo) {
+    return '';
+  }
+
+  return external_Diff_namespaceObject.createPatch('相关信息', rev1.details.rawInfo, rev2.details.rawInfo, rev1.rev.date, rev2.rev.date);
+}
+
+function descriptionDiff(rev1, rev2) {
+  if (rev1.details.description === rev2.details.description) {
+    return '';
+  }
+
+  return external_Diff_namespaceObject.createPatch('简介', rev1.details.description, rev2.details.description, rev1.rev.date, rev2.rev.date);
+}
 ;// CONCATENATED MODULE: external "Diff2Html"
-var external_Diff2Html_namespaceObject = Diff2Html;
+const external_Diff2Html_namespaceObject = Diff2Html;
 ;// CONCATENATED MODULE: ./src/ui.ts
 
 
 function render(diff) {
-    return external_Diff2Html_namespaceObject.html(diff);
+  return external_Diff2Html_namespaceObject.html(diff);
 }
 function show(html) {
-    external_$_namespaceObject('#show-trim21-cn').html(html);
+  external_$_namespaceObject('#show-trim21-cn').html(html);
 }
 function clear() {
-    external_$_namespaceObject('#show-trim21-cn').html('');
+  external_$_namespaceObject('#show-trim21-cn').html('');
 }
-
 ;// CONCATENATED MODULE: ./src/model.ts
 class Commit {
-    constructor(rev, detail) {
-        this.rev = rev;
-        this.details = detail;
-    }
-}
+  constructor(rev, detail) {
+    this.rev = rev;
+    this.details = detail;
+  }
 
+}
 ;// CONCATENATED MODULE: ./src/compare.ts
 
 
 
 
 function compare(revID1, revID2) {
-    clear();
-    show('<h2>loading versions...</h2>');
-    const rev1 = getRevInfo(revID1);
-    const rev2 = getRevInfo(revID2);
-    if (!rev1) {
-        throw new Error(`error finding ${revID1}`);
-    }
-    const ps = [fetchRev(rev1), fetchRev(rev2)];
-    Promise.all(ps)
-        .then((values) => {
-        const d = diff(values[1], values[0]);
-        const rendered = render(d);
-        return show(rendered);
-    })
-        .catch((e) => {
-        console.log(e);
-        show('<h2 style="color:red">loading versions error</h2>');
-    })
-        .finally(() => {
-        document.getElementById('show-trim21-cn')?.scrollIntoView({
-            behavior: 'smooth',
-        });
+  clear();
+  show('<h2>loading versions...</h2>');
+  const rev1 = getRevInfo(revID1);
+  const rev2 = getRevInfo(revID2);
+
+  if (!rev1) {
+    throw new Error(`error finding ${revID1}`);
+  }
+
+  const ps = [fetchRev(rev1), fetchRev(rev2)];
+  Promise.all(ps).then(values => {
+    const d = diff(values[1], values[0]);
+    const rendered = render(d);
+    return show(rendered);
+  }).catch(e => {
+    console.log(e);
+    show('<h2 style="color:red">loading versions error</h2>');
+  }).finally(() => {
+    var _document$getElementB;
+
+    (_document$getElementB = document.getElementById('show-trim21-cn')) === null || _document$getElementB === void 0 ? void 0 : _document$getElementB.scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 }
 const _cache = {};
-async function fetchRev(rev) {
-    if (!rev) {
-        return new Commit({
-            id: '0',
-            comment: '',
-            date: '',
-            url: '',
-        }, {
-            title: '',
-            rawInfo: '',
-            description: '',
-        });
-    }
-    if (!_cache[rev.id]) {
-        const res = await fetch(rev.url);
-        _cache[rev.id] = new Commit(rev, parseRevDetails(await res.text()));
-    }
-    return _cache[rev.id];
-}
 
+async function fetchRev(rev) {
+  if (!rev) {
+    return new Commit({
+      id: '0',
+      comment: '',
+      date: '',
+      url: ''
+    }, {
+      title: '',
+      rawInfo: '',
+      description: ''
+    });
+  }
+
+  if (!_cache[rev.id]) {
+    const res = await fetch(rev.url);
+    _cache[rev.id] = new Commit(rev, parseRevDetails(await res.text()));
+  }
+
+  return _cache[rev.id];
+}
 ;// CONCATENATED MODULE: ./src/index.ts
 
 
 
 
+
 async function main() {
-    console.log('start bgm-wiki-rev-diff UserScript');
-    await initUI();
+  console.log('start bgm-wiki-rev-diff UserScript');
+  await initUI();
 }
+
 const style = `
 <style>
 #show-trim21-cn .d2h-code-line {
@@ -217,79 +232,91 @@ ul#pagehistory > li > * {
 }
 </style>
 `;
-async function initUI() {
-    external_$_namespaceObject('#columnInSubjectA > hr.board').after(style + '<div id="show-trim21-cn"></div>');
-    const diff2htmlStyle = await GM.getResourceUrl('diff2html');
-    external_$_namespaceObject('head').append(`<link rel='stylesheet' type='text/css' href='${diff2htmlStyle}' />`);
-    const s = external_$_namespaceObject('#pagehistory li');
-    const revs = Array.from(s).map(function (e) {
-        return parseRevEl(external_$_namespaceObject(e))?.id;
-    });
-    s.each(function (index) {
-        const el = external_$_namespaceObject(this);
-        const id = revs[index];
-        if (!id) {
-            el.prepend('<span style="padding-right: 1.4em"> 无法参与比较 </span>');
-            return;
-        }
-        el.prepend(`<input type='radio' class='rev-trim21-cn' name='rev-right' label='select to compare' value='${id}'>`);
-        el.prepend(`<input type='radio' class='rev-trim21-cn' name='rev-left' label='select to compare' value='${id}'>`);
-        const previous = external_namespaceObject.find(revs, Boolean, index + 1) ?? '';
-        el.prepend(`(<a href='#' data-rev='${id}' data-previous='${previous}' class='l compare-previous-trim21-cn'>显示修改</a>) `);
-    });
-    const typeRevert = {
-        'rev-left': 'rev-right',
-        'rev-right': 'rev-left',
-    };
-    external_$_namespaceObject('input[type="radio"]').on('change', function (e) {
-        const name = e.target.getAttribute('name');
-        if (!name) {
-            return;
-        }
-        const selectName = typeRevert[name];
-        const rev = e.target.getAttribute('value');
-        if (rev) {
-            external_$_namespaceObject(`input[name="${selectName}"][value="${rev}"]`).css('visibility', 'hidden');
-            external_$_namespaceObject(`input[name="${selectName}"][value!="${rev}"]`).css('visibility', 'visible');
-        }
-    });
-    external_$_namespaceObject('.compare-previous-trim21-cn').on('click', function () {
-        const el = external_$_namespaceObject(this);
-        const left = String(el.data('rev'));
-        const right = String(el.data('previous'));
-        compare(left, right);
-        external_$_namespaceObject(`input[name="rev-left"][value="${left}"]`).prop('checked', true);
-        external_$_namespaceObject(`input[name="rev-left"][value!="${left}"]`).prop('checked', null);
-        external_$_namespaceObject(`input[name="rev-right"][value="${left}"]`).css('visibility', 'hidden');
-        external_$_namespaceObject(`input[name="rev-right"][value!="${left}"]`).css('visibility', 'visible');
-        external_$_namespaceObject('input[name="rev-left"]').css('visibility', 'visible');
-        external_$_namespaceObject('input[name="rev-right"]').prop('checked', null);
-        if (right) {
-            external_$_namespaceObject(`input[name="rev-right"][value="${right}"]`).prop('checked', true);
-            external_$_namespaceObject(`input[name="rev-left"][value="${right}"]`).css('visibility', 'hidden');
-        }
-    });
-    external_$_namespaceObject('#columnInSubjectA span.text').append('<a href="#" id="compare-trim21-cn" class="l"> > 比较选中的版本</a>');
-    external_$_namespaceObject('#compare-trim21-cn').on('click', function () {
-        const selectedRevs = getSelectedVersion();
-        compare(selectedRevs[0], selectedRevs[1]);
-    });
-}
-function getSelectedVersion() {
-    const selectedVersion = [];
-    const selectedRev = external_$_namespaceObject('.rev-trim21-cn:checked');
-    if (selectedRev.length < 2) {
-        window.alert('请选中两个版本进行比较');
-        throw new Error();
-    }
-    selectedRev.each(function () {
-        const val = external_$_namespaceObject(this).val();
-        selectedVersion.push(val);
-    });
-    selectedVersion.sort((a, b) => parseInt(b) - parseInt(a));
-    return selectedVersion;
-}
-main().catch(console.error);
 
+async function initUI() {
+  external_$_namespaceObject('#columnInSubjectA > hr.board').after(style + '<div id="show-trim21-cn"></div>');
+  const diff2htmlStyle = await GM.getResourceUrl('diff2html');
+  external_$_namespaceObject('head').append(`<link rel='stylesheet' type='text/css' href='${diff2htmlStyle}' />`);
+  const s = external_$_namespaceObject('#pagehistory li');
+  const revs = Array.from(s).map(function (e) {
+    var _parseRevEl;
+
+    return (_parseRevEl = parseRevEl(external_$_namespaceObject(e))) === null || _parseRevEl === void 0 ? void 0 : _parseRevEl.id;
+  });
+  s.each(function (index) {
+    const el = external_$_namespaceObject(this);
+    const id = revs[index];
+
+    if (!id) {
+      el.prepend('<span style="padding-right: 1.4em"> 无法参与比较 </span>');
+      return;
+    }
+
+    el.prepend(`<input type='radio' class='rev-trim21-cn' name='rev-right' label='select to compare' value='${id}'>`);
+    el.prepend(`<input type='radio' class='rev-trim21-cn' name='rev-left' label='select to compare' value='${id}'>`);
+    const previous = external_namespaceObject.find(revs, Boolean, index + 1) ?? '';
+    el.prepend(`(<a href='#' data-rev='${id}' data-previous='${previous}' class='l compare-previous-trim21-cn'>显示修改</a>) `);
+  });
+  const typeRevert = {
+    'rev-left': 'rev-right',
+    'rev-right': 'rev-left'
+  };
+  external_$_namespaceObject('input[type="radio"]').on('change', function (e) {
+    const name = e.target.getAttribute('name');
+
+    if (!name) {
+      return;
+    }
+
+    const selectName = typeRevert[name];
+    const rev = e.target.getAttribute('value');
+
+    if (rev) {
+      external_$_namespaceObject(`input[name="${selectName}"][value="${rev}"]`).css('visibility', 'hidden');
+      external_$_namespaceObject(`input[name="${selectName}"][value!="${rev}"]`).css('visibility', 'visible');
+    }
+  });
+  external_$_namespaceObject('.compare-previous-trim21-cn').on('click', function () {
+    const el = external_$_namespaceObject(this);
+    const left = String(el.data('rev'));
+    const right = String(el.data('previous'));
+    compare(left, right);
+    external_$_namespaceObject(`input[name="rev-left"][value="${left}"]`).prop('checked', true);
+    external_$_namespaceObject(`input[name="rev-left"][value!="${left}"]`).prop('checked', null);
+    external_$_namespaceObject(`input[name="rev-right"][value="${left}"]`).css('visibility', 'hidden');
+    external_$_namespaceObject(`input[name="rev-right"][value!="${left}"]`).css('visibility', 'visible');
+    external_$_namespaceObject('input[name="rev-left"]').css('visibility', 'visible');
+    external_$_namespaceObject('input[name="rev-right"]').prop('checked', null);
+
+    if (right) {
+      external_$_namespaceObject(`input[name="rev-right"][value="${right}"]`).prop('checked', true);
+      external_$_namespaceObject(`input[name="rev-left"][value="${right}"]`).css('visibility', 'hidden');
+    }
+  });
+  external_$_namespaceObject('#columnInSubjectA span.text').append('<a href="#" id="compare-trim21-cn" class="l"> > 比较选中的版本</a>');
+  external_$_namespaceObject('#compare-trim21-cn').on('click', function () {
+    const selectedRevs = getSelectedVersion();
+    compare(selectedRevs[0], selectedRevs[1]);
+  });
+}
+
+function getSelectedVersion() {
+  const selectedVersion = [];
+  const selectedRev = external_$_namespaceObject('.rev-trim21-cn:checked');
+
+  if (selectedRev.length < 2) {
+    window.alert('请选中两个版本进行比较');
+    throw new Error();
+  }
+
+  selectedRev.each(function () {
+    const val = external_$_namespaceObject(this).val();
+    selectedVersion.push(val);
+  });
+  selectedVersion.sort((a, b) => parseInt(b) - parseInt(a));
+  return selectedVersion;
+}
+
+main().catch(console.error);
 /******/ })()
 ;
